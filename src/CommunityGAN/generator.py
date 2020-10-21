073e30bbd43c08c67ee8778b39752a8b75a87fba
Bipasha-banerjee <bips.ban@gmail.com>
2020-10-21 12:58:44 -0400

    fork of Community GAN

---
+++ src/CommunityGAN/generator.py
@@ -0,0 +1,32 @@
+import tensorflow as tf
+
+
+class Generator(object):
+    def __init__(self, n_node, node_emd_init, config):
+        self.n_node = n_node
+        self.node_emd_init = node_emd_init
+        self.motif_size = config.motif_size
+        self.max_value = config.max_value
+
+        with tf.compat.v1.variable_scope('generator'):
+            self.embedding_matrix = tf.compat.v1.get_variable(name="embedding",
+                                                    shape=self.node_emd_init.shape,
+                                                    initializer=tf.constant_initializer(self.node_emd_init),
+                                                    trainable=True)
+
+        self.motifs = tf.compat.v1.placeholder(tf.int32, shape=[None, config.motif_size])
+        self.reward = tf.compat.v1.placeholder(tf.float32, shape=[None])
+
+        self.node_embedding = tf.nn.embedding_lookup(self.embedding_matrix, self.motifs)  # Batch * motif_size * embedding_size
+        self.score = tf.reduce_sum(tf.reduce_prod(self.node_embedding, axis=1), axis=1)
+        self.p = 1 - tf.exp(-self.score)
+        self.p = tf.clip_by_value(self.p, 1e-5, 1)
+       
+        self.loss = -tf.reduce_mean((self.p) * (self.reward))
+        optimizer = tf.optimizers.Adam(config.lr_gen)
+        print("here")
+        # model = tf.keras.Sequential()
+        # var_list_fn = lambda: model.trainable_weights
+        # print(type(var_list_fn))
+        self.g_updates = optimizer.minimize(self.loss)
+        self.clip_op = tf.assign(self.embedding_matrix, tf.clip_by_value(self.embedding_matrix, 0, self.max_value))
